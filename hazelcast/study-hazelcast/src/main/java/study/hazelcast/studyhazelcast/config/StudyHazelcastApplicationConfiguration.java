fcbfd8fa883acad154d1ebb28dda896e9539cae0
thainq <thainq@vng.com.vn>
2021-01-29 19:19:28 +0700

    udp

--- hazelcast/study-hazelcast/src/main/java/study/hazelcast/studyhazelcast/config/StudyHazelcastApplicationConfiguration.java
+++ hazelcast/study-hazelcast/src/main/java/study/hazelcast/studyhazelcast/config/StudyHazelcastApplicationConfiguration.java
@@ -26,19 +26,21 @@ public class StudyHazelcastApplicationConfiguration {
         Config hazelCastConf = new Config();
         hazelCastConf.getNetworkConfig().setPort(hazelcastProperties.port).setPortAutoIncrement(false);
         hazelCastConf.setProperty("hazelcast.shutdownhook.policy", "GRACEFUL");
-        JoinConfig join = hazelCastConf.getNetworkConfig().getJoin();
-        join.getMulticastConfig().setEnabled(false);
-        join.getTcpIpConfig().setEnabled(true).clear().setMembers(Arrays.asList(hazelcastProperties.tcpIpMembers.split(";")));
-        hazelCastConf.setInstanceName(hazelcastProperties.instanceName);
+//        JoinConfig join = hazelCastConf.getNetworkConfig().getJoin();
+//        join.getMulticastConfig().setEnabled(false);
+//        join.getTcpIpConfig().setEnabled(true).clear().setMembers(Arrays.asList(hazelcastProperties.tcpIpMembers.split(";")));
+//        hazelCastConf.setInstanceName(hazelcastProperties.instanceName);
 
         SerializationConfig serializationConfig = new SerializationConfig();
         serializationConfig.addPortableFactory(1, new OrderEntityPortableFactory());
         hazelCastConf.setSerializationConfig(serializationConfig);
 
-//        hazelCastConf.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
-//        hazelCastConf.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true)
-//                .setProperty("namespace", hazelcastProperties.namespace)
-//                .setProperty("service-name", hazelcastProperties.serviceName);
+        hazelCastConf.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+        hazelCastConf.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true)
+                .setProperty("namespace", hazelcastProperties.namespace)
+                .setProperty("service-name", hazelcastProperties.serviceName)
+                .setProperty("api-token", "xxxxx")
+                .setProperty("ca-certificate", "xxxxx");
         return Hazelcast.newHazelcastInstance(hazelCastConf);
     }
 }
