ae181b6390d1f52ba5389e4683b182390ec6fbb8
Thai Nguyen <nguyenquocthai0407@gmail.com>
2025-04-21 23:54:25 +0700

    agent

---
+++ agent/langgraph/main.py
@@ -0,0 +1,222 @@
+import getpass
+import os
+from bs4 import BeautifulSoup as Soup
+from langchain_community.document_loaders.recursive_url_loader import RecursiveUrlLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from typing import List
+from typing_extensions import TypedDict
+from langgraph.graph import END, StateGraph, START
+
+
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+
+_set_env("OPENAI_API_KEY")
+_set_env("ANTHROPIC_API_KEY")
+
+
+# LCEL docs
+url = "https://python.langchain.com/docs/concepts/lcel/"
+loader = RecursiveUrlLoader(
+    url=url, max_depth=20, extractor=lambda x: Soup(x, "html.parser").text
+)
+docs = loader.load()
+
+# Sort the list based on the URLs and get the text
+d_sorted = sorted(docs, key=lambda x: x.metadata["source"])
+d_reversed = list(reversed(d_sorted))
+concatenated_content = "\n\n\n --- \n\n\n".join(
+    [doc.page_content for doc in d_reversed]
+)
+
+code_gen_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are a coding assistant with expertise in LCEL, LangChain expression language. \n 
+    Here is a full set of LCEL documentation:  \n ------- \n  {context} \n ------- \n Answer the user 
+    question based on the above provided documentation. Ensure any code you provide can be executed \n 
+    with all required imports and variables defined. Structure your answer with a description of the code solution. \n
+    Then list the imports. And finally list the functioning code block. Here is the user question:""",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+)
+
+class code(BaseModel):
+    """Schema for code solutions to questions about LCEL."""
+
+    prefix: str = Field(description="Description of the problem and approach")
+    imports: str = Field(description="Code block import statements")
+    code: str = Field(description="Code block not including import statements")
+
+expt_llm = "gpt-4o-mini"
+llm = ChatOpenAI(temperature=0, model=expt_llm)
+code_gen_chain_oai = code_gen_prompt | llm.with_structured_output(code)
+
+# question = "How do I build a RAG chain in LCEL?"
+# solution = code_gen_chain_oai.invoke(
+#     {"context": concatenated_content, "messages": [("user", question)]}
+# )
+# print(solution)
+# print('done')
+
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        error : Binary flag for control flow to indicate whether test error was tripped
+        messages : With user question, error messages, reasoning
+        generation : Code solution
+        iterations : Number of tries
+    """
+
+    error: str
+    messages: List
+    generation: str
+    iterations: int
+
+# Max tries
+max_iterations = 3
+# Reflect
+# flag = 'reflect'
+flag = "do not reflect"
+
+def generate(state: GraphState):
+
+    print("---GENERATING CODE SOLUTION---")
+
+    messages = state["messages"]
+    iterations = state["iterations"]
+    error = state["error"]
+
+    if error == "yes":
+        messages += [
+            (
+                "user",
+                "Now, try again. Invoke the code tool to structure the output with a prefix, imports, and code block:",
+            )
+        ]
+
+    # Solution
+    code_solution = code_gen_chain_oai.invoke(
+        {"context": concatenated_content, "messages": messages}
+    )
+    messages += [
+        (
+            "assistant",
+            f"{code_solution.prefix} \n Imports: {code_solution.imports} \n Code: {code_solution.code}",
+        )
+    ]
+
+    iterations = iterations + 1
+    return {"generation": code_solution, "messages": messages, "iterations": iterations}
+
+def code_check(state: GraphState):
+    print("---CHECKING CODE SOLUTION---")
+
+    messages = state["messages"]
+    code_solution = state["generation"]
+    iterations = state["iterations"]
+
+    imports = code_solution.imports
+    code = code_solution.code
+
+    try:
+        exec(imports)
+    except Exception as e:
+        print("---CODE IMPORT CHECK: FAILED---")
+        error_message = [("user", f"Your solution failed the import test: {e}")]
+        messages += error_message
+        return {
+            "generation": code_solution,
+            "messages": messages,
+            "iterations": iterations,
+            "error": "yes",
+        }
+
+    try:
+        exec(imports + "\n" + code)
+    except Exception as e:
+        print("---CODE BLOCK CHECK: FAILED---")
+        error_message = [("user", f"Your solution failed the code execution test: {e}")]
+        messages += error_message
+        return {
+            "generation": code_solution,
+            "messages": messages,
+            "iterations": iterations,
+            "error": "yes",
+        }
+
+    # No errors
+    print("---NO CODE TEST FAILURES---")
+    return {
+        "generation": code_solution,
+        "messages": messages,
+        "iterations": iterations,
+        "error": "no",
+    }
+
+def reflect(state: GraphState):
+    
+    print("---GENERATING CODE SOLUTION---")
+    messages = state["messages"]
+    iterations = state["iterations"]
+    code_solution = state["generation"]
+
+    reflections = code_gen_chain_oai.invoke(
+        {"context": concatenated_content, "messages": messages}
+    )
+
+    messages += [("assistant", f"Here are reflections on the error: {reflections}")]
+    return {"generation": code_solution, "messages": messages, "iterations": iterations}
+
+
+def decide_to_finish(state: GraphState):
+    error = state["error"]
+    iterations = state["iterations"]
+
+    if error == "no" or iterations == max_iterations:
+        print("---DECISION: FINISH---")
+        return "end"
+    else:
+        print("---DECISION: RE-TRY SOLUTION---")
+        if flag == "reflect":
+            return "reflect"
+        else:
+            return "generate"
+
+workflow = StateGraph(GraphState)
+
+# Define the nodes
+workflow.add_node("generate", generate)  # generation solution
+workflow.add_node("check_code", code_check)  # check code
+workflow.add_node("reflect", reflect)  # reflect
+
+# Build graph
+workflow.add_edge(START, "generate")
+workflow.add_edge("generate", "check_code")
+
+workflow.add_conditional_edges(
+    "check_code",
+    decide_to_finish,
+    {
+        "end": END,
+        "reflect": "reflect",
+        "generate": "generate",
+    },
+)
+
+workflow.add_edge("reflect", "generate")
+app = workflow.compile()
+
+question = "How can I directly pass a string to a runnable and use it to construct the input needed for my prompt?"
+solution = app.invoke({"messages": [("user", question)], "iterations": 0, "error": ""})
+
+
+print(solution)
